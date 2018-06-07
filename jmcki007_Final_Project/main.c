77f97902a9e0e51f36c69e55b546fe19fccf9a61
Thorbane <mckinlyjames@gmail.com>
2018-06-06 17:40:18 -0700

    Got basic game logic implemented

--- jmcki007_Final_Project/main.c
+++ jmcki007_Final_Project/main.c
@@ -9,7 +9,7 @@
 #include "timer.h"
 #include "LED.h"
 
-#define tasksNum 5
+#define tasksNum 7
 
 typedef struct task {
     int state; // Current state of the task
@@ -37,12 +37,16 @@ const unsigned long tasksPeriodGCD = 1;
 unsigned char disp_on = 1;
 const unsigned char BOARD_COLOR[3] = {1,0,0};
 const unsigned char CURSOR_COLOR[3] = {2,3,3};
+const unsigned char POSSIBLE_MOVE_COLOR[3] = {1,1,1};
 const unsigned char PLAYR_COLOR[2][3] = {{0,0,2}, {0,2,0}};
 const unsigned char CURSOR_START[2] = {3,3};
+unsigned char Possible_Moves[4][2];
+unsigned char num_possible;
 unsigned char cursor_on;
 unsigned char cursor_player = 0;
 unsigned char piece_to_move = 25;
-unsigned char piece_selected, piece_moved, cursor_moved;    // Flags
+unsigned char piece_selected_flag, piece_moved, cursor_moved;    // Flags
+unsigned char piece_jumped = 25;  // if set to valid index in pieces array, move will take the piece at index out of play
 unsigned char select_flag, reset_flag, up_flag, down_flag, right_flag, left_flag = 0; // Input Flags
 unsigned char cursor_position[2];
 
@@ -59,6 +63,15 @@ void TimerISR() {
         }
         tasks[i].elapsedTime += tasksPeriodGCD;
     }
+}
+
+void ADC_init() {
+    ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+    // ADEN: setting this bit enables analog-to-digital conversion.
+    // ADSC: setting this bit starts the first conversion.
+    // ADATE: setting this bit enables auto-triggering. Since we are
+    //        in Free Running Mode, a new conversion will trigger whenever
+    //        the previous conversion completes.
 }
 
 void set_frame()
@@ -81,11 +94,21 @@ void set_frame()
         }
     }
 	
-	if (piece_selected && (piece_to_move<25))
+	if (piece_selected_flag && (piece_to_move<24))
 	{
 		set_pixel(pieces[piece_to_move].pos[0], pieces[piece_to_move].pos[1], PLAYR_COLOR[pieces[piece_to_move].player][0]+1,
 		 PLAYR_COLOR[pieces[piece_to_move].player][1]+1, PLAYR_COLOR[pieces[piece_to_move].player][2]+1);
 	}
+    
+    //Display possible moves
+    
+    if (piece_selected_flag)
+    {
+        for (unsigned char i = 0; i<num_possible; i++)
+        {
+            set_pixel(Possible_Moves[i][0], Possible_Moves[i][1], POSSIBLE_MOVE_COLOR[0], POSSIBLE_MOVE_COLOR[1], POSSIBLE_MOVE_COLOR[2]);
+        }
+    }
     
     //Display Cursor
     
@@ -154,7 +177,7 @@ unsigned char select_piece()
                 if (pieces[i].player == cursor_player)
                 {
                     piece_to_move = i;
-					piece_selected = 1;
+					piece_selected_flag = 1;
                     return 1;
                 }
             }
@@ -164,15 +187,89 @@ unsigned char select_piece()
     return 0;
 }
 
-unsigned char is_valid_move(unsigned char p_x, unsigned char p_y, unsigned char c_x, unsigned char c_y)
+unsigned char space_occupied(unsigned char pos[2])
 {
-    return 1;
+    if (pos[0]>7 || pos[1]>7)
+    {
+        return 1;
+    }
+    
+    else
+    {
+        for (unsigned char i =0 ; i<24; i++)
+        {
+            if (pieces[i].pos[0] == pos[0] || pieces[i].pos[1] == pos[1])
+            {
+                return 1;
+            }
+        }
+    }
+    
+    return 0;
+}
+
+unsigned char find_moves_for_piece()
+{
+    unsigned char temp[4][2]; // temporarily store possible move positions for checking
+    unsigned char index = 0;
+    if (!pieces[piece_to_move].king)
+    {
+        if (pieces[piece_to_move].player == 0)
+        {
+            if (pieces[piece_to_move].pos[0]>0)
+            {
+                temp[index][0] = pieces[piece_to_move].pos[0]-1;
+                temp[index][1] = pieces[piece_to_move].pos[1]-1;
+                index++;
+            }
+            if (pieces[piece_to_move].pos[0]<7)
+            {
+                temp[index][0] = pieces[piece_to_move].pos[0]+1;
+                temp[index][1] = pieces[piece_to_move].pos[1]-1;
+                index++;
+            }
+            // First 12 pieces in the array are player 0
+            for (unsigned char i = 0; i<12; i++)
+            {
+                for (unsigned char j = 0; j<index; j++)
+                {
+                    if ((pieces[i].pos[0] == temp[j][0]) && (pieces[i].pos[1] == temp[j][1]))
+                    {
+                        temp[j][0] = 8;  // set position to invalid value to indicate it's invalid
+                    }
+                }
+            }
+        }
+    }
+    unsigned char count = 0;
+    for (unsigned char i = 0; i<index; i++)
+    {
+        if (temp[i][0]<8)
+        {
+            Possible_Moves[count][0] = temp[i][0];
+            Possible_Moves[count][1] = temp[i][1];
+            count++;
+        }
+    }
+    
+    return count;
 }
 
 void move_piece()
 {
 	pieces[piece_to_move].pos[0] = cursor_position[0];
 	pieces[piece_to_move].pos[1] = cursor_position[1];
+    
+    if (pieces[piece_to_move].pos[1] == 0 && pieces[piece_to_move].player == 0)
+    {
+        pieces[piece_to_move].king = 1;
+    }
+    
+    if (pieces[piece_to_move].pos[1] == 7 && pieces[piece_to_move].player == 1)
+    {
+        pieces[piece_to_move].king = 1;
+    }
+    piece_moved = 1;
 }
 
 enum ON_OFF_states{INIT, ON, OFF};
@@ -192,7 +289,8 @@ int tick_cursor(int state)
                 if(select_piece())
                 {
                     state = SELECT_PIECE;
-                    piece_selected = 1;
+                    num_possible = find_moves_for_piece();
+                    piece_selected_flag = 1;
                 }
                 else
                 {
@@ -209,27 +307,53 @@ int tick_cursor(int state)
 			{
 				state = SELECT_PIECE;
 			}
-			else if (select_flag && cursor_position[0] == pieces[piece_selected].pos[0] && cursor_position[1] == pieces[piece_selected].pos[1])
+			else if (select_flag && cursor_position[0] == pieces[piece_to_move].pos[0] && cursor_position[1] == pieces[piece_to_move].pos[1])
 			{
 				state = MOVE;
+				num_possible = 0;
+				piece_selected_flag = 0;
+				piece_to_move = 25;
+				select_flag = 0;
+			}                
+			else if (select_flag)
+			{
+                for (unsigned char i = 0; i<num_possible; i++)
+                {
+                    if (cursor_position[0] == Possible_Moves[i][0] && cursor_position[1] == Possible_Moves[i][1])
+                    {
+                        move_piece();
+                        state = MOVE;
+                        num_possible = 0;
+                        piece_selected_flag = 0;
+                        piece_to_move = 25;
+                        select_flag = 0;
+                        if (piece_jumped < 24)
+                        {
+                            pieces[piece_jumped].in_play = 0;
+                        }
+                        break;
+                    }
+                }
+                if (!piece_moved)
+                {
+                    state = SELECT_PIECE;
+                    select_flag = 0;
+                }                    
+            }
+            else if (reset_flag)
+            {
                 select_flag = 0;
                 piece_to_move = 25;
-                piece_selected = 0;
-			}
-			else
-			{
-				if (is_valid_move(pieces[piece_to_move].pos[0], pieces[piece_to_move].pos[1], cursor_position[0], cursor_position[1]))
-				{
-                    move_piece(piece_to_move);
-					piece_moved = 1;
-                    select_flag = 0;
-                    state = MOVE;
-                    piece_to_move = 25;
-                    piece_selected = 0;
-                    break;
-				}
+                piece_selected_flag = 0;
+                num_possible = 0;
+            }
+            else
+            {
                 state = MOVE;
-            }                
+                num_possible = 0;
+                piece_selected_flag = 0;
+                piece_to_move = 25;
+            }              
             break;
         default:
             state = INIT;
@@ -272,15 +396,15 @@ int tick_cursor(int state)
             up_flag = down_flag = left_flag = right_flag = 0;
             break;
         
-        case SELECT_PIECE:
+        case SELECT_PIECE:  // Much the same actions as the MOVE state, except cursor is tethered to within 2 tiles of selected piece
             if (up_flag != down_flag)
             {
-                if(up_flag && (cursor_position[1]>0))
+                if(up_flag && (cursor_position[1]>0) && cursor_position[1]>(pieces[piece_to_move].pos[1]-2))
                 {
                     cursor_position[1]--;
                     cursor_moved = 1;
                 }
-                if(down_flag && (cursor_position[1]<7))
+                if(down_flag && (cursor_position[1]<7) && cursor_position[1]<(pieces[piece_to_move].pos[1]+2))
                 {
                     cursor_position[1]++;
                     cursor_moved = 1;
@@ -289,12 +413,12 @@ int tick_cursor(int state)
             
             if (right_flag != left_flag)
             {
-                if(left_flag && (cursor_position[0]>0))
+                if(left_flag && (cursor_position[0]>0) && cursor_position[0]>(pieces[piece_to_move].pos[0]-2))
                 {
                     cursor_position[0]--;
                     cursor_moved = 1;
                 }
-                if(right_flag && (cursor_position[0]<7))
+                if(right_flag && (cursor_position[0]<7) && cursor_position[0]<(pieces[piece_to_move].pos[0]+2))
                 {
                     cursor_position[0]++;
                     cursor_moved = 1;
@@ -336,6 +460,97 @@ int tick_button_input(int state)
     
     return state;
 }      
+
+enum JOYSTICK_STATES{J_INIT, X, Y, SEL};
+
+int tick_joystick_input(int state)
+{
+    unsigned long input = ADC;
+    static long x, y;
+    switch (state)
+    {
+        case J_INIT:
+            x = 0;
+            y = 0;
+            ADMUX = 0;
+            state = X;
+            break;
+        case X:
+            if (!(PINA & 0x40))
+            {
+                state = SEL;
+                select_flag = 1;
+                break;
+            }
+            if (input < 462)
+            {
+                x += (input - 512);
+                if (x < -475)
+                {
+                    left_flag = 1;
+                    x = 0;
+                }
+            }
+            else if (input > 562)
+            {
+                x += (input - 512);
+                if (x > 475)
+                {
+                    right_flag = 1;
+                    x = 0;
+                }
+            }
+            else
+            {
+                x = 0;
+            }
+            state = Y;
+            ADMUX = 1;
+            break;
+        case Y:
+            if (!(PINA & 0x40))
+            {
+                state = SEL;
+                select_flag = 1;
+                break;
+            }
+            if (input < 462)
+            {
+                y += (input - 512);
+                if (y < -475)
+                {
+                    down_flag = 1;
+                    y = 0;
+                }
+            }
+            else if (input > 562)
+            {
+                y += (input - 512);
+                if (y > 475)
+                {
+                    up_flag = 1;
+                    y = 0;
+                }
+            }
+            else
+            {
+                y = 0;
+            }
+            state = X;
+            ADMUX = 1;
+            break;
+        case SEL:
+            if (PINA & 0x40)
+            {
+                state = X;
+                x = 0;
+                y = 0;
+            }
+            else {state = SEL;}
+            break;
+    }
+    return state;
+}    
     
 int tick_cursor_blink(int state)
 {
@@ -395,6 +610,70 @@ int tick_cursor_blink(int state)
             break;
     }
     
+    return state;
+}
+
+enum GAME_STATES{GAME_INIT, PLAYER_1_TURN, PLAYER_2_TURN, PLAYER_1_WIN, PLAYER_2_WIN, RESET};
+
+int tick_game(int state)
+{
+    switch (state)
+    {
+        case GAME_INIT:
+            game_init();
+            cursor_player = 0;
+            state = PLAYER_1_TURN;
+            break;
+        case PLAYER_1_TURN:
+            if (piece_jumped)
+            {
+                piece_moved = 0;  //set flag to 0
+                state = PLAYER_1_TURN;  // continue turn
+            }
+            if (piece_moved)
+            {
+                piece_moved = 0;
+                state = PLAYER_2_TURN;
+            }
+            break;
+        case PLAYER_2_TURN:
+            if (piece_jumped)
+            {
+                piece_moved = 0;  //set flag to 0
+                state = PLAYER_2_TURN;  // continue turn
+            }
+            if (piece_moved)
+            {
+                piece_moved = 0;
+                state = PLAYER_1_TURN;
+            }
+            break;
+        case PLAYER_1_WIN:
+            if (!reset_flag)
+            {
+                state = PLAYER_1_WIN;
+            }
+            else
+            {
+                state = GAME_INIT;
+            }
+            break;
+            
+        case PLAYER_2_WIN:
+            if (!reset_flag)
+            {
+                state = PLAYER_2_WIN;
+            }
+            else
+            {
+                state = GAME_INIT;
+            }
+            break;
+            
+        default:
+            state = GAME_INIT;
+            break;
+    }
     return state;
 }
 
@@ -496,6 +775,11 @@ int main()
     tasks[i].TickFct = &tick_button_input;
     i++;
     tasks[i].state = -1;
+    tasks[i].period = 250;
+    tasks[i].elapsedTime = 250;
+    tasks[i].TickFct = &tick_joystick_input;
+    i++;
+    tasks[i].state = -1;
     tasks[i].period = 500;
     tasks[i].elapsedTime = 500;
     tasks[i].TickFct = &tick_cursor;
@@ -508,6 +792,11 @@ int main()
     tasks[i].state = -1;
     tasks[i].period = 500;
     tasks[i].elapsedTime = 500;
+    tasks[i].TickFct = &tick_game;
+    i++;
+    tasks[i].state = -1;
+    tasks[i].period = 500;
+    tasks[i].elapsedTime = 500;
     tasks[i].TickFct = &set_display;
     i++;
     tasks[i].state = -1;
@@ -516,7 +805,7 @@ int main()
     tasks[i].TickFct = &tick_display;
     
     game_init();
-	//ADC_init();
+	ADC_init();
 	
 	TimerSet(1);
 	TimerOn();
